7ea68e11409d742eba8afd91bd29d6fa16884e6e
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-07-10 18:39:26 +0300

    Connect Postgres database.

--- instagram/settings.py
+++ instagram/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER': 'peterken',
+        'PASSWORD': 'pa55'
     }
 }
 
