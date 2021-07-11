1f85f77cdea58570eab9158a4d42e4821d1cbf71
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-07-11 09:27:55 +0300

    Add static files.

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),  
 ]
