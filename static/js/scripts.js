42472f587d162048a4a5208349633dc1dc1531d6
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-07-12 18:10:07 +0300

    Add image details modal.

---
+++ static/js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.post-content').click(function(){
+        url = $(this).attr('data-url');
+        $('#imageModal .img-container img').attr('src', url);
+    })
+})
