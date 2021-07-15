01c43eba5336188e9ac366dddcb20adf71d009b6
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-07-15 11:05:31 +0300

    Add modal link.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,10 +3,25 @@ $(document).ready(function(){
         url = $(this).attr('data-url');
         username = $(this).attr('data-username');
         profilepic = $(this).attr('data-profilepic')
+        caption = $(this).attr('data-caption')
 
         $('#imageModal .img-container img').attr('src', url);
         $('#imageModal .post-header-profilepic img').attr('src', profilepic);
+        $('#imageModal .post-header-username #caption').text(caption);
         $('#imageModal .post-header-username small strong').text(username);
+        
+    })
+    $('#viewComments').click(function () {
+        url = $(this).attr('data-url');
+        username = $(this).attr('data-username');
+        profilepic = $(this).attr('data-profilepic')
+        caption = $(this).attr('data-caption')
+
+        $('#imageModal .img-container img').attr('src', url);
+        $('#imageModal .post-header-profilepic img').attr('src', profilepic);
+        $('#imageModal .post-header-username #caption').text(caption);
+        $('#imageModal .post-header-username small strong').text(username);
+        
     })
     
 })
