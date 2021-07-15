2fd587115764dddf0b60bd7c77bead2ab70e9b91
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-07-15 10:27:38 +0300

    Improve image expand modal.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
     $('.post-content').click(function(){
         url = $(this).attr('data-url');
+        username = $(this).attr('data-username');
+        profilepic = $(this).attr('data-profilepic')
+
         $('#imageModal .img-container img').attr('src', url);
+        $('#imageModal .post-header-profilepic img').attr('src', profilepic);
+        $('#imageModal .post-header-username small strong').text(username);
     })
     
 })
